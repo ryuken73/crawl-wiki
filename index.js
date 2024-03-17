3e723e6ca5c7b5f3796a603236944a011d2df85a
ryuken73 <ryuken73@gmail.com>
2024-03-17 19:24:13 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const { chromium } = require('playwright');
+
+const openHeadless = async (options) => {
+  const browser = await chromium.launchPersistentContext('', {
+    headless: false
+  })
+  const page = await browser.newPage()
+  return page
+}
+
+const getLinInList = async (locator, options) => {
+  return locator.getByRole('listitem').getByRole('link', options).all();
+}
+
+
+const main = async () => {
+  const KOR_ACTOR_URL = 'https://namu.wiki/w/%EB%B0%B0%EC%9A%B0/%ED%95%9C%EA%B5%AD';
+  const PERSON_LIST_REGEXP = /(^[가-힣]{2,4}$)|([가-힣]{2,4} - .*$)/;
+
+  const page = await openHeadless()
+  await page.goto(KOR_ACTOR_URL)
+  const personsLocators = await getLinInList(page, {name: PERSON_LIST_REGEXP});
+  console.log('1. number of persons:', personsLocators.length);
+  for(const person of personsLocators){
+    console.log('name:', await person.innerText())
+  }
+}
+
+main()
