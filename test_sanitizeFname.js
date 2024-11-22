686d9dbd6a5946353cb378c1e1bf45f28a832283
ryuken73 <ryuken73@gmail.com>
2024-11-22 14:22:29 +0900

    update: add script to sanitize filename

---
+++ test_sanitizeFname.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const basePath = 'd:/002.Code/002.node/crawl-wiki/images/가수/한국';
+
+const sanitizeFname = (fname) => {
+  const toRemoveChars = /['"() ]/g;
+  const invalidChars = /[\\/:*?"<>\|\-]/g;
+  return fname.replace(toRemoveChars, '').replace(invalidChars, '_').replace(/_+/g, '_');
+}
+
+fs.readdir(basePath, (err, files) => {
+  files.filter(file => /txt$/.test(file)).map(file => {
+    console.log(sanitizeFname(file))
+  })
+})
