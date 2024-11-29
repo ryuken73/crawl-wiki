1e6d7b0848494121a97f052cb439aaa45c585e24
ryuken73 <ryuken73@gmail.com>
2024-11-29 14:07:34 +0900

    update: working version on class based browser

--- test_browser.js
+++ test_browser.js
@@ -1,20 +1,89 @@
+const path = require('path');
 const {create, setLevel} = require('./lib/logger')();
 const logger = create({logFile:'crawl_wiki.log'});
 const createBrowser = require('./lib/browser');
+const fileUtil = require('./lib/util');
 const crawl_config = require('./crawl_config.json');
+const {
+  dbGetNextSeqId
+} = require('./lib/queries');
 
 const WIKI_URL = 'https://namu.wiki'
 const URL = 'https://namu.wiki/w/%EB%B6%84%EB%A5%98:%ED%95%9C%EA%B5%AD%20%EC%97%AC%EC%84%B1%20%EB%AA%A8%EB%8D%B8';
 const URL_2000 = 'https://namu.wiki/w/%EB%B6%84%EB%A5%98:2000%EB%85%84%20%EC%B6%9C%EC%83%9D'
-const WIKI_IDs = {
+const URL_2006 = 'https://namu.wiki/w/%EB%B6%84%EB%A5%98:2006%EB%85%84%20%EC%B6%9C%EC%83%9D'
+const URL_2021 = 'https://namu.wiki/w/%EB%B6%84%EB%A5%98:2021%EB%85%84%20%EC%B6%9C%EC%83%9D'
+const WIKI_CATEGORY_IDS = {
   'DOCUMENTS': 'category-문서',
   'CLASSES': 'category-분류',
 }
-const filterRule = /[0-9]/;
+const DB_SEQUENCE_NAME = 'person.content_id_seq'
+// const filterRule = /[0-9]/;
 // const filterRule = /.*/;
-const {SAVE_PATH} = crawl_config;
+const {
+  SAVE_PATH,
+  TEMP_PATH
+} = crawl_config;
+const {
+  utilDelBlankLine,
+  utilSaveToFile,
+  utilMoveFile
+} = fileUtil
 
-const processWikiList = async (browser, list) => {
+const CRAWL_START_URLS = [
+  {
+    startPageUrl: 'https://namu.wiki/w/%EB%B0%B0%EC%9A%B0/%ED%95%9C%EA%B5%AD',
+    personIdPrefix: '배우_한국',
+    // presonPageLinksRegExp: /.*/, 
+    // pageLinksRegExp: /(^[가-힣]{2,4}$)|([가-힣]{2,4} - .*$)/,
+    personPageLinksRegExp: /감우성/
+  }
+]
+
+const genUniqId = (prefix, id, fullName, idType) => {
+  const type = idType === 'content' ?  "C" : "I";
+  return `${prefix}_${type}_${id}_${fullName}`;
+}
+const getNextId = async (options) => {
+  const {personIdPrefix, listText, idType} = options;
+  const dbNextSequence = await dbGetNextSeqId(DB_SEQUENCE_NAME);
+  const uniqId = genUniqId(personIdPrefix, dbNextSequence, listText, idType);
+  return uniqId;
+}
+const saveImageToTemp = async (imgBody, listText) => {
+  try {
+    const timeStamp = Date.now();
+    const fname = `${timeStamp}_${listText}.webp`
+    const tempFname = path.join(TEMP_PATH, fname);
+    const result = await utilSaveToFile(imgBody, tempFname)
+    if(result === true){
+      return {
+        saveImageResult: result,
+        tmpImageName: tempFname
+      }
+    } else {
+      return {
+        saveImageToTemp: false
+      }
+    }
+  } catch (err) {
+      throw err;
+  }
+}
+
+const getImageFname = (imageId, subDir, listText) => {
+  return path.join(SAVE_PATH, subDir, `${imageId}.webp`)
+}
+const renameImageFile = async () => {}
+const dbSaveContent = async () => {}
+
+const sanitizeFname = (fname) => {
+  const toRemoveChars = /['"() ]/g;
+  const invalidChars = /[\\/:*?"<>\|\-]/g;
+  return fname.replace(toRemoveChars, '').replace(invalidChars, '_').replace(/_+/g, '_');
+}
+
+const processWikiList = async (browser, list, personIdPrefix) => {
   for(let item of list){
     console.log('item text:', await item.textContent());
   }
@@ -33,31 +102,53 @@ const processWikiList = async (browser, list) => {
         continue
       }
       const contents = await browser.wikiGetContents(personPage);
-      const imgUrl = await browser.wikiGetImageUrl(personPage, listText);
+      const {imgUrl, imgBody} = await browser.wikiGetImageData(personPage);
+
+      const {saveImageResult, tmpImageName} = await saveImageToTemp(imgBody, listText)
+      if(saveImageResult === false){
+        continue
+      }
+      const imageId = await getNextId({personIdPrefix, listText, idType: 'image'})
+      logger.info('imageId =', imageId)
+      const imageFname = getImageFname(imageId, personIdPrefix, listText);
+      logger.info(`move file from=${tmpImageName}, to=${imageFname}`)
+      const renameSuccess = await utilMoveFile(tmpImageName, imageFname);
+      if(renameSuccess === false){
+        continue
+      }
+      const contentId = await getNextId({personIdPrefix, listText, idType: 'content'})
+      // await dbSaveContent(contentId)
+      console.log('contents:')
+      console.log(utilDelBlankLine(contents))
       console.log(imgUrl);
       
       logger.info('[end]', listText)
       browser.closeChildPage();
     } catch (err) {
+      console.error(err.message)
       logger.error('fail to process person', listText);
       console.error('fail to process person', listText);
       browser.closeChildPage();
-      continue;
     }
   }
   return
 }
 
-async function main() {
+async function main(crawlInfo) {
   const options = {
     savePath: SAVE_PATH,
   }
-  const browser = await createBrowser(URL, options);
+  const {
+    startPageUrl,
+    personPageLinksRegExp,
+    personIdPrefix
+  } = crawlInfo
+  const browser = await createBrowser(startPageUrl, options);
   const list = await browser.getListWithLinkInArray({
-    regexp: filterRule,
-    id: WIKI_IDs.DOCUMENTS
+    regexp: personPageLinksRegExp,
+    // id: WIKI_CATEGORY_IDS.DOCUMENTS
   });
-  await processWikiList(browser, list)
+  await processWikiList(browser, list, personIdPrefix)
 
   console.log('done')
   // const buttons = await browser.getButtonsInLocatorArray();
@@ -70,12 +161,12 @@ async function main() {
     }
     await browser.clickLinkAndWait(nextButton);
     const list = await browser.getListWithLinkInArray({
-      regexp: filterRule,
-      id: WIKI_IDs.DOCUMENTS
+      regexp: personPageLinksRegExp,
+      // id: WIKI_CATEGORY_IDS.DOCUMENTS
     });
-    await processWikiList(browser, list)
+    await processWikiList(browser, list, personIdPrefix)
   }
   process.exit();
 }
 
-main();
+main(CRAWL_START_URLS[0]);
