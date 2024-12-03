72e5781bb6b36730f3c60e238bb5c468e88913ac
ryuken73 <ryuken73@gmail.com>
2024-12-03 14:41:12 +0900

    update: check validation of webp image file when db loads

--- addDatabase.js
+++ addDatabase.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const {
   utilReadJsonFile,
-  utilGetJsonFileList,
+  utilGetJsonWebpFileList,
+  utilIsWebpFile,
   wikiSeparateFileType,
   wikiGetNameFromFname,
-  wikiGetImageFileWithName
+  wikiGetImageFileWithName,
+  wikiGetWebpImageFileWithName
 } = require('./lib/util')
 const {
   dbInsertContent,
@@ -16,13 +18,14 @@ const BASE_FOLDER = 'D:/002.Code/002.node/crawl-wiki/work';
 const WORKING_FOLDER_NAME = '2024-12-02 16.44.51';
 
 async function main(){
-  let contentsFiles, imageFiles;
+  let contentsFiles, imageFiles, webpFiles;
   try {
     const workingFoler = path.join(BASE_FOLDER, WORKING_FOLDER_NAME);
-    const flist = await utilGetJsonFileList(workingFoler);
+    const flist = await utilGetJsonWebpFileList(workingFoler);
     const files = wikiSeparateFileType(flist);
     contentsFiles = files[0];
     imageFiles = files[1];
+    webpFiles = files[2];
   } catch (err) {
     console.error(err)
     process.exit()
@@ -30,9 +33,16 @@ async function main(){
 
   for(let contentFile of contentsFiles){
     const name = wikiGetNameFromFname(contentFile);
+    const webpFileWithName = wikiGetWebpImageFileWithName(webpFiles, name)
+    if(webpFileWithName.length > 1){
+      throw new Error(`webp file for ${name} should be one but ${webpFileWithName.length}`)
+    } 
+    if(!await utilIsWebpFile(webpFileWithName[0])){
+      throw new Error(`${webpFileWithName[0]} is not valid webp file type`);
+    }
     const imageFilesWithName = wikiGetImageFileWithName(imageFiles, name);
     if(imageFilesWithName.length > 1){
-        throw new Error(`image meta file for ${name} should be one but ${imageFilesWithName.length}`)
+      throw new Error(`image meta file for ${name} should be one but ${imageFilesWithName.length}`)
     }
     const imageFile = imageFilesWithName[0];
     console.log('processing..', name, imageFile);
