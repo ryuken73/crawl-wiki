f5a95433e6b0fe9e959c9115768ff433d6554d58
ryuken73 <ryuken73@gmail.com>
2024-12-03 16:19:16 +0900

    update: move content, image, webp file to proper directories after processing

--- addDatabase.js
+++ addDatabase.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const {create, setLevel} = require('./lib/logger')();
 const {
   utilReadJsonFile,
   utilGetJsonWebpFileList,
   utilIsWebpFile,
+  utilMoveFile,
   wikiSeparateFileType,
   wikiGetNameFromFname,
   wikiGetImageFileWithName,
@@ -15,40 +17,65 @@ const {
 } = require('./lib/queries');
 
 const BASE_FOLDER = 'D:/002.Code/002.node/crawl-wiki/work';
-const WORKING_FOLDER_NAME = '2024-12-02 16.44.51';
+const WORKING_FOLDER_NAME = '2024-12-03 11.47.42';
+const BASE_IMAGE_FOLDER = 'D:/002.Code/002.node/crawl-wiki/images';
 
 async function main(){
   let contentsFiles, imageFiles, webpFiles;
+  const workingFoler = path.join(BASE_FOLDER, WORKING_FOLDER_NAME);
+  logger = create({logFile:path.join(workingFoler, 'addDatabase.log')});
   try {
-    const workingFoler = path.join(BASE_FOLDER, WORKING_FOLDER_NAME);
     const flist = await utilGetJsonWebpFileList(workingFoler);
     const files = wikiSeparateFileType(flist);
     contentsFiles = files[0];
     imageFiles = files[1];
     webpFiles = files[2];
+    logger.info(`file counts: contents[${contentsFiles.length}], imageMeta[${imageFiles.length}], webp[${webpFiles.length}]`)
   } catch (err) {
-    console.error(err)
+    console.log(err.stack)
+    logger.error(err)
     process.exit()
+  }
+
+  const moveFinal = async (fname, subDir) => {
+    const finalSavePath = path.join(BASE_IMAGE_FOLDER, subDir);
+    const finalSaveFname = path.join(finalSavePath, path.basename(fname));
+    await utilMoveFile(fname, finalSaveFname);
+  }
+  const moveDone = async (fname) => {
+    const doneSavePath = path.join(workingFoler, 'done');
+    const doneSaveFname = path.join(doneSavePath, path.basename(fname))
+    await utilMoveFile(fname, doneSaveFname);
+  }
+  const moveInvalidWebp = async (fname) => {
+    const savePath = path.join(workingFoler, 'invalid_webp');
+    const saveFname = path.join(savePath, path.basename(fname))
+    await utilMoveFile(fname, saveFname);
   }
 
   for(let contentFile of contentsFiles){
     const name = wikiGetNameFromFname(contentFile);
     const webpFileWithName = wikiGetWebpImageFileWithName(webpFiles, name)
-    if(webpFileWithName.length > 1){
+    console.log(webpFileWithName)
+    if(webpFileWithName.length > 1 || webpFileWithName.length === 0){
       throw new Error(`webp file for ${name} should be one but ${webpFileWithName.length}`)
     } 
-    if(!await utilIsWebpFile(webpFileWithName[0])){
-      throw new Error(`${webpFileWithName[0]} is not valid webp file type`);
-    }
     const imageFilesWithName = wikiGetImageFileWithName(imageFiles, name);
+    console.log(imageFilesWithName);
     if(imageFilesWithName.length > 1){
       throw new Error(`image meta file for ${name} should be one but ${imageFilesWithName.length}`)
     }
     const imageFile = imageFilesWithName[0];
-    console.log('processing..', name, imageFile);
+    logger.info('processing..', name);
     const contentJson = await utilReadJsonFile(contentFile)
     const imageJson = await utilReadJsonFile(imageFile)
+    const webpFile = webpFileWithName[0];
     try {
+      if(!await utilIsWebpFile(webpFile)){
+        // throw new Error(`${webpFile} is not valid webp file type`);
+        logger.error(`${webpFile} is not valid webp file type`);
+        throw new Error('INVALID_WEBP');
+      }
       const resultContent = await dbInsertContent(contentJson);
       if(resultContent === undefined){
         throw new Error(`error to insert content for ${name}`)
@@ -57,8 +84,20 @@ async function main(){
       if(resultImage === undefined){
         throw new Error(`error to insert image for ${name}`)
       }
+      const {imageSubDir} = imageJson;
+      await moveFinal(webpFile, imageSubDir)
+      await moveDone(imageFile);
+      await moveDone(contentFile);
     } catch (err) {
-      console.error(err)
+      if(err.message === 'INVALID_WEBP'){
+        logger.error('move all file to invalid folder');
+        await moveInvalidWebp(imageFile)
+        await moveInvalidWebp(contentFile)
+        await moveInvalidWebp(webpFile)
+        continue;
+      }
+      console.log(err.stack)
+      logger.error(err.message)
     }
   }
   
