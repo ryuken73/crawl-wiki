6aba58a0c36d08a9fec7c92a8081723d039a882c
ryuken73 <ryuken73@gmail.com>
2024-12-25 11:47:08 +0900

    update: 3D and 2D forced network works!

--- UI/js/dataHandlers.js
+++ UI/js/dataHandlers.js
@@ -14,7 +14,11 @@ const isNewNode = (row, prevResult) => {
 }
 
 export const mkNetworkData = (rows, sourceId, prevResult={nodes:[], links:[]}) => {
-  return rows.reduce((acct, row, index) => {
+  const gData = rows.reduce((acct, row, index) => {
+    if(row.content_id === null){
+      return acct
+    }
+      console.log(`${row.backlink_text} is not just backlink. add `)
     const newNodes = isNewNode(row, prevResult) ?
     [
       ...acct.nodes,
@@ -40,4 +44,26 @@ export const mkNetworkData = (rows, sourceId, prevResult={nodes:[], links:[]}) =
       links: newLinks
     }
   }, prevResult)
+  console.log('gData=', gData)
+  // if(gData.links.length === 0){
+  //   return gData;
+  // }
+  // gData.links.forEach(link => {
+  //   console.log(link)
+  //   // const a = gData.nodes[link.source];
+  //   // const b = gData.nodes[link.target];
+  //   const a = gData.nodes.find(node => node.id === link.source);
+  //   const b = gData.nodes.find(node => node.id === link.target);
+  //   console.log(a, b)
+  //   !a.neighbors && (a.neighbors = []);
+  //   !b.neighbors && (b.neighbors = []);
+  //   a.neighbors.push(b);
+  //   b.neighbors.push(a);
+
+  //   !a.links && (a.links = []);
+  //   !b.links && (b.links = []);
+  //   a.links.push(link);
+  //   b.links.push(link);
+  // });
+  return gData;
 }
