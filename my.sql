2a9601270d75f4bc83e6806d22f5a78b3accacbb
ryuken73 <ryuken73@gmail.com>
2025-01-03 10:47:37 +0900

    etc: save useful sqls

--- my.sql
+++ my.sql
@@ -1,6 +1,10 @@
 SELECT * FROM person.backlink_count order by backlink_count desc;
 
-select * from person.contents
+select content_id, content_name, additional_info_raw, additional_info from person.contents
+
+select to_tsvector('열혈사제2와 관련된 배우들')
+
+
 
 select b.backlink_text
 from person.backlinks b
@@ -8,8 +12,26 @@ join person.contents_backlinks cb on b.backlink_id = cb.backlink_id
 join person.contents c on cb.content_id = c.content_id
 where c.content_name = '신민아' 
 
+-- backlinkId from contentId
+select backlink_id from person.backlinks b
+join person.contents c on c.content_url = b.backlink_url
+where c.content_id ='가수_한국_C_004301_유재석'
+
+-- contents 중에서 backlinks에 수집되지 않은 것들 조회
+select c.content_id, c.content_name, b.backlink_id from person.contents c
+left join person.backlinks b on b.backlink_url = c.content_url
+where b.backlink_url is null
+
+-- update primary_category of content
+select substring(content_id, 1,5) from person.contents_clone;
+
+update person.contents set primary_category = (
+select substring(content_id, 1,5) from person.contents co
+where co.content_id = person.contents.content_id
+)
+
 -- 신민아 node 눌렀을 때 관련된 backlinks 리턴 (각 backlink가 person인지 표시)
-select b.backlink_text, b.backlink_url,
+select b.backlink_text, b.backlink_url, b.content_ref_count,
 case when exists(
 	select 1 from person.contents cc 
 	where cc.content_url = b.backlink_url
@@ -18,16 +40,18 @@ end as isPerson
 from person.backlinks b
 join person.contents_backlinks cb on b.backlink_id = cb.backlink_id
 join person.contents c on cb.content_id = c.content_id
-where c.content_name = '신민아' -- content_id로 고쳐야겠지? 
+-- where c.content_name = '신민아' -- content_id로 고쳐야겠지? 
+where c.content_id = '가수_한국_C_004301_유재석'
+order by content_ref_count desc
 
 -- 신민아 node 눌렀을 때 관련된 backlinks들의 text, url 그리고 backlink들의 backlink 갯수(만약 있다면)를 리턴
-select b.backlink_text, b.backlink_url,
+select b.backlink_text, b.backlink_url, b.content_ref_count,
 (select backlink_count 
  from person.contents cc, person.backlink_count bc 
  where cc.content_id = bc.content_id
  and cc.content_url = b.backlink_url
  )
- as count
+ as backink_count
 from person.backlinks b
 join person.contents_backlinks cb on b.backlink_id = cb.backlink_id
 join person.contents c on cb.content_id = c.content_id
@@ -73,7 +97,7 @@ LEFT JOIN backlink_counts bc
 -- WHERE c.content_name = '신민아';
 where c.content_id = '배우_한국_C_000001_감우성'
 
-------------------------------------------------------------
+
 
 -- 1984년이라는 node 눌렀을 때 그것과 연결된 person return
 select c.content_name
@@ -87,10 +111,38 @@ from person.contents c, person.contents_backlinks cb
 where c.content_id = cb.content_id and c.content_name = '이일화'
 
 
--------------------------------------------------------------
--- backlink incremental crawling 시 대상 조회: backlink_count에 없는 contents가져오기
 
+-- backlink incremental crawling 시 대상 조회: backlink_count에 없는 contents가져오기
 select c.content_id
 from person.contents c left outer join person.backlink_count bc
 on c.content_id = bc.content_id
 where bc.backlink_count is null
+
+
+-- backlink별 역참조 카운트 조회 (live)
+select  cb.backlink_id, b.backlink_text, count(1) as count 
+from person.contents_backlinks cb 
+join person.backlinks b
+on cb.backlink_id = b.backlink_id
+group by 1,2 
+order by count desc
+
+-- backlink별 역참조 카운트 조회 (집계)
+select backlink_text, content_ref_count from person.backlinks
+order by 2 desc
+
+-- backlinks count update
+update person.backlinks b set content_ref_count = (
+	select count(*) 
+	from person.contents_backlinks cb 
+	where b.backlink_id = cb.backlink_id
+)
+
+-- 가장 역참조가 많은 backlink 조회
+select  cb.backlink_id, b.backlink_text, count(1) as count 
+from person.contents_backlinks cb 
+join person.backlinks b
+on cb.backlink_id = b.backlink_id
+group by 1,2 
+having count(*) > 1000
+order by count desc
